6e1c04ab671986db906ce790576e39e506ec3b30
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2021-07-10 04:03:51 -0700

    Front end casi terminado con back end

---
+++ app/src/main/java/ambrosio/armando/practica11_myfeelings_ambrosios/utilities/Emociones.kt
@@ -0,0 +1,4 @@
+package ambrosio.armando.practica11_myfeelings_ambrosios.utilities
+
+data class Emociones(var nombre: String, var porcentaje:Float,
+                     var color: Int, var total: Float)
